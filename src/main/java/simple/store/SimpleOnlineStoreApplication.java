7c9d62fed65f3a35585ef29ac704d89ea17ecb36
TiagoCa <tiagocaraujo@gmail.com>
2021-11-11 10:31:28 -0300

    First version.

---
+++ src/main/java/simple/store/SimpleOnlineStoreApplication.java
@@ -0,0 +1,13 @@
+package simple.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleOnlineStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleOnlineStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/simple/store/SimpleOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package simple.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
